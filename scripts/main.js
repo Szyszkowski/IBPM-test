29d879e7b8c9d86e5ebcb7d536bfed72ac5234c7
szyszkowski <szysza0x@gmail.com>
2016-05-09 20:01:58 +0200

    first working version

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,63 @@
 /**
  * Created by szyszka on 09.05.16.
  */
+var map;
+var infowindow;
+var pyrmont;
+var markers = [];
+function initMap() {
+  pyrmont = new google.maps.LatLng(54.372755, 18.635715);
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: pyrmont,
+    zoom: 11
+  });
+}
+function update(form) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  debugger;
+  service.nearbySearch({
+    name: form.name.value || undefined,
+    location: pyrmont,
+    radius: form.distance.value * 1000,
+    type: [form.type.value]
+  }, callback);
+}
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+  markers.push(marker);
+
+  google.maps.event.addListener(marker, 'click', function () {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
+
+$('#form').validator().on('submit', function (e) {
+  debugger;
+  if (e.isDefaultPrevented()) {
+    // handle the invalid form...
+  } else {
+    update(this);
+    return false
+  }
+});
+
